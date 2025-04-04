897c4621dbb4361f52b7beaf5029ed31460e8fdf
Sergey Chafonov <c.sergey@karmy.su>
2025-04-04 17:58:45 +0300

    lesson2

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'new project';
+let screens = 'Простые, Сложные, Интерактивные';
+let screenPrice = 200;
+let rollback = 50;
+let fullPrice = 150000;
+let adaptive = true;
 
-alert("какоето сообщение");
-
-console.log("text in console");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log('Стоимость верстки экранов' + screenPrice + 'рублей/ долларов/гривен/юани \n Стоимость разработки сайта' + fullPrice + 'рублей/ долларов/гривен/юани')
+console.log(screens.toLowerCase().split())
